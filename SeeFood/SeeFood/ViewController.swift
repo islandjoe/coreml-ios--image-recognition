4797dc6f3d48913f935b2a59c340e4876457b87d
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-11 13:35:24 +0300

    UI implementation

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+  @IBOutlet weak var imageView: UIImageView!
+  
+  let imagePicker = UIImagePickerController()
+  
+  @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    present(imagePicker, animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    if let userPickedImage = info[ UIImagePickerControllerOriginalImage ] as? UIImage {
+      imageView.image = userPickedImage
+    }
+    
+    imagePicker.dismiss(animated: true, completion: nil)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    imagePicker.delegate      = self
+    imagePicker.sourceType    = .camera
+    imagePicker.allowsEditing = false
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
